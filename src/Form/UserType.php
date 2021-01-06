11a3e833eead0d4efb04f2dda3ba43500260fff0
Magictruks <turchini.axel@gmail.com>
2021-01-06 19:00:05 +0100

    patch role select dans user form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\CallbackTransformer;
 
 class UserType extends AbstractType
 {
@@ -28,7 +29,7 @@ class UserType extends AbstractType
                 ]
                 ,
                 'expanded' => false,
-                'multiple' => true,
+                'multiple' => false,
                 'required' => true,
                 )
             )
@@ -45,6 +46,18 @@ class UserType extends AbstractType
                 'attr' => ['class' => 'form-check']
             ))
         ;
+
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                     // transform the array to a string
+                     return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                     // transform the string back to an array
+                     return [$rolesString];
+                }
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
